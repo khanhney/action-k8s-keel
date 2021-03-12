cecaf6219efd363f8382f9dba6619565c17aa91a
khanhney <khanhney.dev@gmail.com>
2021-03-12 22:52:52 +0700

    initial simple http + test

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+let dotenv  = require('dotenv');
+
+app.get('/check-health', (req, res) => {
+    res.json({ status: 'active', message: 'healthy' });
+});
+
+app.get('/add/:numA/:numB', (req, res) => {
+    let { numA, numB } = req.params;
+    let resultAdd = +numA + +numB;
+
+    return res.json({ result: resultAdd });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
+
+module.exports = app;
---
+++ test/resp.test.js
@@ -0,0 +1,32 @@
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
+let should = chai.should();
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Pets', () => {
+    beforeEach((done) => {
+        //Before each test we empty the database in your case
+        done();
+    });
+    /*
+     * Test the /GET route
+     */
+    describe('/GET function ADD', () => {
+        it('it should GET result function Add', (done) => {
+            let obj = {
+                numA: 1, numB: 2, expected: 3
+            }
+            chai.request(server)
+                .get(`/add/${obj.numA}/${obj.numB}`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('result').eql(obj.expected);
+                    done();
+                });
+        });
+    });
+});
